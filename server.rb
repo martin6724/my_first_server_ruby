77d78e27c144e75ac35583075d3845e6e87ca96d
Michelle Martin <martin.michelleann@gmail.com>
2018-04-14 17:19:13 -0400

    updating  heroku 3

--- server.rb
+++ server.rb
@@ -1,11 +1,37 @@
 require 'sinatra'
-set :public_folder, File.dirname(__FILE__) + '/static/index.html'
+require 'httparty'
 
-get '/' do
-html :index
+set :public_folder, File.dirname(__FILE__) + '/static'
+
+get '/urlsplit/:kitty' do
+	url = 'https://google.com:443/?name=wyncode&day=friday#foo'
+	return params[:kitty] + "\n"
+	# some parts of a URL
+	# scheme
+	# domain
+	# port
+	# path
+	# query string
+	# fragment
+	pieces = url.split('://')
+	scheme, *rest = pieces
+	second_piece, = rest
+	third, frag = second_piece.split('#')
+	{
+		scheme: scheme,
+		rest: rest,
+		third: third,
+		frag: frag,
+	}.to_json
+
 end
 
+get '/party' do
+	url = 'http://example.com'
+	kitty = HTTParty.get(url)
+	kitty.body
+end
 
-get "/sinatra" do
-  "Hello Sinatra"
+get '/' do
+	return "Hello, from Heroku."
 end
